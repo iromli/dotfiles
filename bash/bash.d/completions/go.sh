7efde5e4b20251f4484dcf65f12a2ad61794f4bf
iromli <isman.firmansyah@gmail.com>
2022-04-21 02:31:33 +0700

    add new completion scripts

--- bash/bash.d/completions/go.sh
+++ bash/bash.d/completions/go.sh
@@ -1,250 +1,104 @@
-# install in /etc/bash_completion.d/ or your personal directory
-
-complete -f -X '!*.8' 8l
-complete -f -X '!*.6' 6l
-complete -f -X '!*.5' 5l
-complete -f -X '!*.go' 8g 6g 5g gofmt gccgo
-
-_go_importpath()
-{
-  echo "$(compgen -W "$(go list all) all std" -- "$1")"
-}
-
-_go()
-{
-  # TODO: Only allow flags before other arguments. run already does
-  # this.
-
-  local cur=`_get_cword`
-  local prev="${COMP_WORDS[COMP_CWORD-1]}"
-
-  local cmd="${COMP_WORDS[1]}"
-
-  local cmds="build clean doc fix fmt get
-    install list run test tool version vet"
-  local addhelp="gopath importpath remote
-    testflag testfunc"
-  local other="help"
-
-  if [ "$COMP_CWORD" == 1 ]; then
-    for opt in $cmds; do
-      if [[ "$opt" == "$cmd" ]]; then
-        COMPREPLY=("$opt")
-        return
-      fi
+get_go_pkgs_in() {(
+    IFS=" "
+    gopath="$1"
+    word="$2"
+    match="$word"
+    case "$word" in
+	"."|"./"*)
+	    match="$(go list -e .)${word:1}"
+	    ;;
+	".."|"../"*)
+	    match="$(go list -e ..)${word:2}"
+	    ;;
+    esac
+    for d in "$gopath"/src/"$match"*; do
+	[[ "$d" == *"*" ]] && continue
+	[[ -d "$d" ]] || continue
+	d="${d/$gopath\/src\/} "
+	d="$word${d:${#match}}"
+	echo -n "$d"
     done
-  fi
+    if [ "${match: -1}" != "/" ]; then
+	for d in "$gopath"/src/"$match"/*; do
+	    [[ "$d" == *"*" ]] && continue
+	    [[ -d "$d" ]] || continue
+	    d="${d/$gopath\/src\/}/ "
+	    d="$word${d:${#match}}"
+	    echo -n "$d"
+	done
+    fi
+)}
 
-  case "$cmd" in
-    'build')
-      case "$prev" in
-        '-o')
-          _filedir
-          ;;
-        '-p')
-          ;;
-        *)
-          if [[ "$cur" == -* ]]; then
-            COMPREPLY=($(compgen -W "-a -n -o -p -v -x" -- "$cur"))
-          else
-            local found=0
-            for ((i=0; i < ${#COMP_WORDS[@]}; i++)); do
-              case "$i" in
-                0|1|"$COMP_CWORD")
-                  continue
-                  ;;
-              esac
-              local opt="${COMP_WORDS[i]}"
-              if [[ "$opt" != -* ]]; then
-                if [[ "$opt" == *.go && -f "$opt" ]]; then
-                  found=1
-                  break
-                else
-                  found=2
-                  break
-                fi
-              fi
-            done
-            case "$found" in
-              0)
-                _filedir go
-                COMPREPLY+=(`_go_importpath "$cur"`)
-                ;;
-              1)
-                _filedir go
-                ;;
-              2)
-                COMPREPLY=(`_go_importpath "$cur"`)
-                ;;
-            esac
-          fi
-          ;;
-      esac
-      ;;
-    'clean')
-      if [[ "$cur" == -* ]]; then
-        COMPREPLY=($(compgen -W "-i -r -n -x" -- "$cur"))
-      else
-        COMPREPLY=(`_go_importpath "$cur"`)
-      fi
-      ;;
-    'doc')
-      COMPREPLY=(`_go_importpath "$cur"`)
-      ;;
-    'fix')
-      COMPREPLY=(`_go_importpath "$cur"`)
-      ;;
-    'fmt')
-      COMPREPLY=(`_go_importpath "$cur"`)
-      ;;
-    'get')
-      case "$prev" in
-        '-p')
-          ;;
-        *)
-          if [[ "$cur" == -* ]]; then
-            COMPREPLY=($(compgen -W "-a -d -fix -n -p -u -v -x" -- "$cur"))
-          else
-            COMPREPLY=(`_go_importpath "$cur"`)
-          fi
-          ;;
-      esac
-      ;;
-    'install')
-      case "$prev" in
-        '-p')
-          ;;
-        *)
-          if [[ "$cur" == -* ]]; then
-            COMPREPLY=($(compgen -W "-a -n -p -v -x" -- "$cur"))
-          else
-            COMPREPLY=(`_go_importpath "$cur"`)
-          fi
-          ;;
-      esac
-      ;;
-    'list')
-      case "$prev" in
-        '-f')
-          ;;
-        *)
-          if [[ "$cur" == -* ]]; then
-            COMPREPLY=($(compgen -W "-e -f -json" -- "$cur"))
-          else
-            COMPREPLY=(`_go_importpath "$cur"`)
-          fi
-          ;;
-      esac
-      ;;
-    'run')
-      if [[ "$cur" == -* && "$prev" != *.go ]]; then
-        COMPREPLY=($(compgen -W "-a -n -x" -- "$cur"))
-      else
-        _filedir
-      fi
-      ;;
-    'test') # TODO: Support for testflags.
-      case "$prev" in
-        '-file')
-          _filedir go
-          ;;
-        '-p')
-          ;;
-        *)
-          if [[ "$cur" == -* ]]; then
-            COMPREPLY=($(compgen -W "-c -file -i -p -x" -- "$cur"))
-          else
-            COMPREPLY=(`_go_importpath "$cur"`)
-          fi
-          ;;
-        esac
-      ;;
-    'tool')
-      if [ "$COMP_CWORD" == 2 ]; then
-        COMPREPLY=($(compgen -W "$(go tool)" -- "$cur"))
-      else
-        case "${COMP_WORDS[2]}" in
-          [568]a) # TODO: Implement something.
-            #_go_tool_568a
-            ;;
-          [568]c) # TODO: Implement something.
-            #_go_tool_568c
-            ;;
-          [568]g) # TODO: Implement something.
-            #_go_tool_568g
-            ;;
-          [568]l) # TODO: Implement something.
-            #_go_tool_568l
-            ;;
-          'api') # TODO: Implement something.
-            #_go_tool_api
-            ;;
-          'cgo') # TODO: Implement something.
-            #_go_tool_cgo
-            ;;
-          'cov') # TODO: Implement something.
-            #_go_tool_cov
-            ;;
-          'dist') # TODO: Implement something.
-            #_go_tool_dist
-            ;;
-          'ebnflint') # TODO: Implement something.
-            #_go_tool_ebnflint
-            ;;
-          'fix') # TODO: Implement something.
-            #_go_tool_fix
-            ;;
-          'gotype') # TODO: Implement something.
-            #_go_tool_gotype
-            ;;
-          'nm') # TODO: Implement something.
-            #_go_tool_nm
-            ;;
-          'pack') # TODO: Implement something.
-            #_go_tool_pack
-            ;;
-          'pprof') # TODO: Implement something.
-            #_go_tool_pprof
-            ;;
-          'prof') # TODO: Implement something.
-            #_go_tool_prof
-            ;;
-          'vet') # TODO: Implement something.
-            #_go_tool_vet
-            ;;
-          'yacc') # TODO: Implement something.
-            #_go_tool_yacc
-            ;;
-        esac
-        if [[ "$cur" == -* ]]; then
-          COMPREPLY=($(compgen -W "${COMPREPLY[*]} -h" -- "$cur"))
-        fi
-      fi
-      ;;
-    'version')
-      ;;
-    'vet')
-      if [[ "$cur" == -* ]]; then
-        :
-      else
-        COMPREPLY=(`_go_importpath "$cur"`)
-      fi
-      ;;
-    'help')
-      if [ "$COMP_CWORD" == 2 ]; then
-        COMPREPLY=($(compgen -W "$cmds $addhelp" -- "$cur"))
-      fi
-      ;;
-    *)
-      if [ "$COMP_CWORD" == 1 ]; then
-        COMPREPLY=($(compgen -W "$cmds $other" -- "$cur"))
-      else
-        _filedir
-      fi
-      ;;
-  esac
+get_go_pkgs_inkgs_dup() {(
+    word=$1
+    IFS=":"
+    for g in $GOPATH; do
+        get_go_pkgs_in "$g" "$word"
+    done
+)}
+
+
+get_go_pkgs() {
+    get_go_pkgs_dup $@ | sort | uniq
 }
 
-complete $filenames -F _go go
+get_go_cmds() {
+    for i in build clean env fix fmt generate get install list run test tool version vet save vendor mod; do
+        [[ $i == $1* ]] && echo $i
+    done
+}
 
-# vim:ts=2 sw=2 et syn=sh
+get_go_tools() {
+    for i in $(go tool -n); do
+        [[ $i == $1* ]] && echo $i
+    done
+}
+
+get_go_mod() {
+   for i in download edit graph init tidy vendor verify why; do
+       [[ $i == $1* ]] && echo $i
+   done
+}
+
+go_pkg_complete() {
+    set -- $COMP_LINE
+    shift
+
+    while [[ $1 == -* ]]; do
+          shift
+    done
+
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    if grep -q '^\(install\|build\|list\|get\|test\|generate\|vet\|save\|vendor\)$' <<< $1; then
+        COMPREPLY=( $(compgen -W "$(get_go_pkgs $cur)" -- $cur) )
+        return
+    elif grep -q '^\(tool\)$' <<< $1; then
+        COMPREPLY=( $(compgen -W "$(get_go_tools $cur)" -- $cur) )
+        return
+    elif grep -q '^\(mod\)$' <<< $1; then
+        COMPREPLY=( $(compgen -W "$(get_go_mod $cur)" -- $cur) )
+        return
+    elif grep -q '^\(run\|fmt\)$' <<< $1; then
+        compopt -o default
+        COMPREPLY=()
+        return
+    fi
+
+    case "$1" in
+        run)
+            COMPREPLY=( $(compgen -A file -G "$cur**/*.go" -- $cur) )
+            return
+	    ;;
+    esac
+
+    [[ $2 ]] && return
+
+    COMPREPLY=( $(compgen -W "$(get_go_cmds $cur)" -- $cur) )
+}
+
+wgo_pkg_complete() {
+    GOPATH=$(wgo env GOPATH) go_pkg_complete $@
+}
+
+complete -o nospace -F go_pkg_complete go
+complete -o nospace -F wgo_pkg_complete wgo
